968ad38f327b412bb8ca91c1b5dc95161bee784c
Htoo Aye Lwin <htooayelwinict@gmail.com>
2026-06-15 22:07:39 +0630

    fix(appv22): keep extension registry task scoped

--- appV2.2/appv22/extensions/__init__.py
+++
@@ -1,9 +0,0 @@
-from appv22.extensions.base import RuntimeExtension, SkillCard
-from appv22.extensions.registry import ExtensionRegistry, ResolvedExtensions
-
-__all__ = [
-    "ExtensionRegistry",
-    "ResolvedExtensions",
-    "RuntimeExtension",
-    "SkillCard",
-]
